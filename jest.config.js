faf04fb9a1ecbf62a2290b6e35ebdb89100b513e
Marcus Vinicius Marques <marcus.marques@wisereducacao.com>
2022-02-20 03:43:19 -0300

    feat: add RTL and test configs

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,10 @@ module.exports = {
   testEnvironment: "jsdom",
   testPathIgnorePatterns: ["/node_modules/", "/build/"],
   collectCoverage: true,
-  collectCoverageFrom: ["src/**/*.ts(x)"],
+  collectCoverageFrom: ["src/**/*.ts(x)?"],
   setupFilesAfterEnv: ["<rootDir>/.jest/setup.ts"],
+  modulePaths: ["<rootDir>/src/"],
+  transform: {
+    "^.+\\.(js|jsx|ts|tsx)$": ["babel-jest"],
+  },
 };
